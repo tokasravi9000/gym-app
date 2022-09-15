4ac3c9d2aa069f85c10b0fa5c69a55d9991486e9
ravi <ravitokas.9000@gmail.com>
2022-09-16 02:46:48 +0400

    update exerciseVideos file

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import { Typography, Box, Stack } from '@mui/material';
-import Loader from './Loader';
+import React from "react";
+import { Typography, Box, Stack } from "@mui/material";
+import Loader from "./Loader";
 
 const ExerciseVideos = ({ exerciseVideos, name }) => {
   if (!exerciseVideos.length) return <Loader />;
-    
+
   return (
-    <Box sx={{ marginTop: { lg: '203px', xs: '20px' } }} p="20px">
-      <Typography sx={{ fontSize: { lg: '44px', xs: '25px' } }} fontWeight={700} color="#000" mb="33px">
-        Watch <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>{name}</span> exercise videos
+    <Box sx={{ marginTop: { lg: "203px", xs: "20px" } }} p="20px">
+      <Typography
+        sx={{ fontSize: { lg: "44px", xs: "25px" } }}
+        fontWeight={700}
+        color="#000"
+        mb="33px"
+      >
+        Watch{" "}
+        <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
+          {name}
+        </span>{" "}
+        exercise videos
       </Typography>
-      <Stack sx={{ flexDirection: { lg: 'row' }, gap: { lg: '110px', xs: '0px' } }} justifyContent="flex-start" flexWrap="wrap" alignItems="center">
-        
-      </Stack>
+      <Stack
+        sx={{ flexDirection: { lg: "row" }, gap: { lg: "110px", xs: "0px" } }}
+        justifyContent="flex-start"
+        flexWrap="wrap"
+        alignItems="center"
+      ></Stack>
     </Box>
   );
 };
